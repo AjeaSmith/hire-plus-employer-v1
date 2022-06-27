2ec021d69db17836c3d1c70b9006b47a5a92a358
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2022-06-26 23:38:55 -0400

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './routes/home/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<HomePage />}>
+					{/* <Route index element={<JobsPage />} />
+					<Route path="auth/employees" element={<AuthPage />}>
+						<Route index element={<SignIn />} />
+						<Route path="sign-up" element={<Signup />} />
+						...
+					</Route>
+					<Route
+						path="user/profile/:id"
+						element={
+							<PrivateRoute>
+								<ProfilePage />
+							</PrivateRoute>
+						}
+					/> */}
+				</Route>
+				{/* <Route path="*" element={<NoMatch />} /> */}
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ src/routes/home/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface HomePageProps {}
+
+const HomePage: React.FC<HomePageProps> = () => {
+	return <div>Im the home</div>;
+};
+
+export default HomePage;
