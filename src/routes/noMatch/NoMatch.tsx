b8681972ed9874490a14f9190954da3533febdb4
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2022-06-28 11:33:46 -0400

    added no match route

---
+++ src/routes/noMatch/NoMatch.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+	return (
+		<div className="text-3xl text-center my-24 text-white">
+			Sorry, page not found.
+		</div>
+	);
+};
+
+export default NoMatch;
